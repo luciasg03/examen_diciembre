963356c2029bdc9c7aae7846c88e792fc2e7fae9
Lucía Silva <johndoe@example.com>
2021-12-03 11:28:49 +0100

    Certamen

--- premios_nobel.sql
+++ premios_nobel.sql
@@ -1,9 +1,20 @@
 CREATE TABLE Cientifico (
-    Id int,
+    Id int not null PRIMARY KEY,
     Nombre varchar(255),
     Apellidos varchar(255),
     Edad INTEGER,
     id_genero INTEGER
 );
+CREATE Table Genero(
+    id INTEGER NOT NULL PRIMARY KEY,
+    nombre VARCHAR(255)
+);
 
-
+create table Certamen(
+    id_premio INT,
+    id_cientifico INT,
+    dinero FLOAT,
+    anho DATETIME YEAR,
+    id_genero INT,
+    id_labor_cientifica INT
+)
